1550718c24de2b602e5afdb722806fec57a4da01
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2022-11-24 12:16:37 +0100

    Class for file management & better folder structure

---
+++ task1/helpers/file.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * Class for loading HTML files and writing to CSV files
+ */
+class File
+{
+  /**
+   * Load HTML file as string
+   */
+  public static function loadHTML(string $path): string
+  {
+    if (file_exists($path)) {
+      $file = file_get_contents($path);
+
+      if (!$file) throw new Error("File opening failed");
+
+      return $file;
+    } else throw new Error("File does not exists");
+  }
+
+  /**
+   * Convert string to HTML, or throw error on failure
+   */
+  public static function generateDOM(string $file): \DOMDocument
+  {
+    $dom = new DOMDocument();
+
+    /**
+     * The warning has been hidden because it does not change anything.
+     * If everything went well, the HTML is returned, and if not,
+     * the actual error is thrown thanks to if
+     */
+    if (!@$dom->loadHTML($file)) throw new Error("HTML loading failed");
+
+    return $dom;
+  }
+
+  /**
+   * Writes data to CSV file
+   */
+  public static function writeCSV($arr): void
+  {
+    $fp = fopen("./output/result.csv", "w+");
+
+    if ($fp) {
+      echo "<br /><h3>Successfully generated result.csv file</h3>";
+
+      // Write CSV to file
+      fputcsv($fp, array_keys($arr));
+      fputcsv($fp, array_values($arr));
+    } else echo "An error occurred while generating the result.csv file";
+  }
+}
--- task1/index.php
+++ task1/index.php
@@ -14,9 +14,11 @@
   </form>
 
   <?php
+  require_once "./helpers/file.php";
+
   // Handle button press
   if (isset($_POST["submit"])) {
-    $filePath = "./wo_for_parse.html";
+    $filePath = "./data/wo_for_parse.html";
 
     $extracted = [
       "Tracking Number" => null,
@@ -33,85 +35,63 @@
       "Phone" => null
     ];
 
-    if (file_exists($filePath)) {
-      // Load target file
-      $file = file_get_contents($filePath);
+    // Load target file
+    $file = File::loadHTML($filePath);
 
-      if (!$file) throw new Error("File opening failed");
+    // Generate DOM
+    $dom = File::generateDOM($file);
 
-      $dom = new DOMDocument();
+    /** Search for data */
 
-      /**
-       * Convert file from string to HTML, or throw error on failure
-       *
-       * The warning has been hidden because it does not change anything.
-       * If everything went well, the HTML is returned, and if not,
-       * the actual error is thrown thanks to if
-       */
-      if (!@$dom->loadHTML($file)) throw new Error("HTML loading failed");
+    $extracted["Customer"] = trim($dom->getElementById("customer")->nodeValue);
+    $extracted["Tracking Number"] = $dom->getElementById("wo_number")->nodeValue;
+    $extracted["PO Number"] = $dom->getElementById("po_number")->nodeValue;
+    $extracted["Trade"] = $dom->getElementById("trade")->nodeValue;
+    $extracted["Store ID"] = $dom->getElementById("location_name")->nodeValue;
+    $extracted["Town"] = $dom->getElementById("store_id")->nodeValue;
 
-      /** Search for data */
+    // Get address
+    $address = $dom->getElementById("location_address")->nodeValue;
 
-      $extracted["Customer"] = trim($dom->getElementById("customer")->nodeValue);
-      $extracted["Tracking Number"] = $dom->getElementById("wo_number")->nodeValue;
-      $extracted["PO Number"] = $dom->getElementById("po_number")->nodeValue;
-      $extracted["Trade"] = $dom->getElementById("trade")->nodeValue;
-      $extracted["Store ID"] = $dom->getElementById("location_name")->nodeValue;
-      $extracted["Town"] = $dom->getElementById("store_id")->nodeValue;
+    // Replace spaces, blank characters, etc. with a single space
+    $cleanAddress = trim(preg_replace("/\s+/", " ", $address));
+    $addressArr = explode(" ", $cleanAddress);
 
-      // Get address
-      $address = $dom->getElementById("location_address")->nodeValue;
+    // Get State from address
+    $extracted["State"] = $addressArr[sizeof($addressArr) - 2];
 
-      // Replace spaces, blank characters, etc. with a single space
-      $cleanAddress = trim(preg_replace("/\s+/", " ", $address));
-      $addressArr = explode(" ", $cleanAddress);
+    // Get zip code from address
+    $extracted["ZIP Code"] = end($addressArr);
 
-      // Get State from address
-      $extracted["State"] = $addressArr[sizeof($addressArr) - 2];
+    // Convert to float
+    $extracted["NTE"] = floatval(preg_replace("/[^0-9]/", "", $dom->getElementById("nte")->nodeValue));
+    // Convert to float
+    $extracted["Phone"] = floatval(preg_replace("/[^0-9]/", "", $dom->getElementById("location_phone")->nodeValue));
 
-      // Get zip code from address
-      $extracted["ZIP Code"] = end($addressArr);
+    // Get Street from address
+    $townIndex = array_search($extracted["Town"], $addressArr);
 
-      // Convert to float
-      $extracted["NTE"] = floatval(preg_replace("/[^0-9]/", "", $dom->getElementById("nte")->nodeValue));
-      // Convert to float
-      $extracted["Phone"] = floatval(preg_replace("/[^0-9]/", "", $dom->getElementById("location_phone")->nodeValue));
-
-      // Get Street from address
-      $townIndex = array_search($extracted["Town"], $addressArr);
-
-      // Loop through the address until the city is met
-      for ($i = 0; $i < $townIndex; $i++) {
-        $extracted["Street"] .= ' ' . $addressArr[$i];
-      }
-
-      try {
-        // Replace spaces, blank characters, etc. with a single space
-        $cleanDate = trim(preg_replace("/\s+/", " ", $dom->getElementById("scheduled_date")->nodeValue));
-
-        // Transform string into an array
-        $dArr = preg_split("/ |,/", $cleanDate);
-        $formattedDate = $dArr[3] . '-' . date("m", strtotime($dArr[0])) . '-' . $dArr[1] . ' ' . $dArr[4] . ' ' . $dArr[5];
-
-        $extracted["Scheduled Date"] = $formattedDate;
-      } catch (\Throwable $th) {
-        // echo $th;
-        $extracted["scheduleDate"] = null;
-      }
-
-      /** Save as .csv */
-
-      // Open/create file
-      $fp = fopen("./result.csv", "w+");
-
-      if ($fp) {
-        echo "<br /><h3>Successfully generated result.csv file</h3>";
-
-        // Write CSV to file
-        fputcsv($fp, array_keys($extracted));
-        fputcsv($fp, array_values($extracted));
-      } else echo "An error occurred while generating the result.csv file";
+    // Loop through the address until the city is met
+    for ($i = 0; $i < $townIndex; $i++) {
+      $extracted["Street"] .= ' ' . $addressArr[$i];
     }
+
+    try {
+      // Replace spaces, blank characters, etc. with a single space
+      $cleanDate = trim(preg_replace("/\s+/", " ", $dom->getElementById("scheduled_date")->nodeValue));
+
+      // Transform string into an array
+      $dArr = preg_split("/ |,/", $cleanDate);
+      $formattedDate = $dArr[3] . '-' . date("m", strtotime($dArr[0])) . '-' . $dArr[1] . ' ' . $dArr[4] . ' ' . $dArr[5];
+
+      $extracted["Scheduled Date"] = $formattedDate;
+    } catch (\Throwable $th) {
+      // echo $th;
+      $extracted["scheduleDate"] = null;
+    }
+
+    /** Save as .csv */
+    File::writeCSV($extracted);
   }
   ?>
 </body>
